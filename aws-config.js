eb51e7bc5b268f66f177f8a2ef928dde39272dcb
Parlindungan Sinabariba <parlindungan@bukalapak.com>
2020-10-07 11:34:50 +0800

    Update aws-config.js

--- aws-config.js
+++ aws-config.js
@@ -1,15 +1,7 @@
 module.exports = {
-    "accessKeyId": "AKIAIKR6QK4UC5IUPSAA",
-    "secretAccessKey": "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",
+    "accessKeyId": "",
+    "secretAccessKey": "",
     "region": "ap-southeast-1",
     "hostname": "https://kinesis.ap-southeast-1.amazonaws.com/",
     "endpoint": "https://kinesis.ap-southeast-1.amazonaws.com/"
 }
-
-/**
-Access Key ID:
-AKIAJ5HFHWQWECGW7KEA
-Secret Access Key:
-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
-https://kinesis.us-east-2.amazonaws.com/
- */
